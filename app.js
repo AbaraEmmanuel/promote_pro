c18ecf9910d590886b348ef18cacb6e1926c8cec
AbaraEmmanuel <141164356+AbaraEmmanuel@users.noreply.github.com>
2024-08-10 00:09:40 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-// Import Firebase libraries
+// Import Firebase modules
 import { initializeApp } from "firebase/app";
 import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 
 // Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD4DVbIQUzhNSczujsP27MwTE6NfifB8ew",
-  authDomain: "promote-pro-8f9aa.firebaseapp.com",
-  projectId: "promote-pro-8f9aa",
-  storageBucket: "promote-pro-8f9aa.appspot.com",
-  messagingSenderId: "553030063178",
-  appId: "1:553030063178:web:13e2b89fd5c6c628ccc2b3",
-  measurementId: "G-KZ89FN869W"
+    apiKey: "AIzaSyD4DVbIQUzhNSczujsP27MwTE6NfifB8ew",
+    authDomain: "promote-pro-8f9aa.firebaseapp.com",
+    projectId: "promote-pro-8f9aa",
+    storageBucket: "promote-pro-8f9aa.appspot.com",
+    messagingSenderId: "553030063178",
+    appId: "1:553030063178:web:13e2b89fd5c6c628ccc2b3",
+    measurementId: "G-KZ89FN869W"
 };
 
 // Initialize Firebase
@@ -18,18 +18,22 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 window.onload = async function() {
+    console.log('Page loaded');
     if (window.Telegram && window.Telegram.WebApp) {
         const user = window.Telegram.WebApp.initDataUnsafe;
         const userId = user?.user?.id;
         const firstName = user?.user?.first_name || "";
         const lastName = user?.user?.last_name || "";
 
-        // Display the username
+        console.log('User Data:', user);
+        console.log('User ID:', userId);
+
         document.getElementById('userName').textContent = `${firstName} ${lastName}`;
 
         if (userId) {
             try {
                 // Fetch user data from Firestore
+                console.log('Fetching user data...');
                 const userDoc = doc(db, "users", userId.toString());
                 const docSnap = await getDoc(userDoc);
 
