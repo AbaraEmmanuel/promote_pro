10edbd5f713f5dbbeb4cfc52260ec5b2acf4a35b
AbaraEmmanuel <emmanuelabara265@gmail.com>
2024-08-19 23:55:37 +0100

    Update

--- server.js
+++ server.js
@@ -3,11 +3,21 @@ const express = require('express');
 const path = require('path');
 const admin = require('firebase-admin');
 
-// Initialize Firebase Admin with service account
-const serviceAccount = require('./config/serviceAccountKey.json');
+// Initialize Firebase Admin with environment variables
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: 'https://promote-pro-8f9aa-default-rtdb.firebaseio.com/' // Replace with your Firebase Realtime Database URL
+    credential: admin.credential.cert({
+        type: process.env.FIREBASE_TYPE,
+        project_id: process.env.FIREBASE_PROJECT_ID,
+        private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+        private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+        client_email: process.env.FIREBASE_CLIENT_EMAIL,
+        client_id: process.env.FIREBASE_CLIENT_ID,
+        auth_uri: process.env.FIREBASE_AUTH_URI,
+        token_uri: process.env.FIREBASE_TOKEN_URI,
+        auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+        client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+    }),
+    databaseURL: 'https://promote-pro-8f9aa-default-rtdb.firebaseio.com/'
 });
 
 const db = admin.database();
@@ -17,7 +27,6 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware to parse JSON bodies
 app.use(express.json());
-
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
 
