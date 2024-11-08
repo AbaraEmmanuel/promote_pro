7942ad7d6093123a57b3d217aefddda11c598702
AbaraEmmanuel <emmanuelabara265@gmail.com>
2024-11-08 15:22:12 +0100

    Updated app.js

--- public/app.js
+++ public/app.js
@@ -1,19 +1,3 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.x/firebase-app.js";
-import { getFirestore, doc, getDoc, setDoc, updateDoc } from "https://www.gstatic.com/firebasejs/9.x/firebase-firestore.js";
-
-const firebaseConfig = {
-    apiKey: "AIzaSyD4DVbIQUzhNSczujsP27MwTE6NfifB8ew",
-    authDomain: "promote-pro-8f9aa.firebaseapp.com",
-    databaseURL: "https://promote-pro-8f9aa-default-rtdb.firebaseio.com",
-    projectId: "promote-pro-8f9aa",
-    storageBucket: "promote-pro-8f9aa.firebasestorage.app",
-    messagingSenderId: "553030063178",
-    appId: "1:553030063178:web:13e2b89fd5c6c628ccc2b3",
-    measurementId: "G-KZ89FN869W"
-  };
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app)
 window.onload = async function() {
     if (window.Telegram && window.Telegram.WebApp) {
         const user = window.Telegram.WebApp.initDataUnsafe;
@@ -76,45 +60,3 @@ window.onload = async function() {
         console.error('Telegram WebApp is not available');
     }
 };
-
-// Function to initialize user data
-async function initializeUserData(userId) {
-    try {
-        await fetch('/update', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                userId,
-                points: 0,
-                tasksDone: 0,
-                completedTasks: []
-            })
-        });
-    } catch (error) {
-        console.error('Error initializing user data:', error);
-    }
-}
-
-// Function to update user data
-async function updateUserData(userId, points, tasksDone) {
-    try {
-        const completedTasks = Array.from(document.querySelectorAll('.task.completed')).map(task => task.id);
-
-        await fetch('/update', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                userId,
-                points,
-                tasksDone,
-                completedTasks
-            })
-        });
-    } catch (error) {
-        console.error('Error updating user data:', error);
-    }
-}
